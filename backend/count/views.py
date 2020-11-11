218081d18ae95aeb9ebfb2d8cdd1e670eb992f42
challengingcoder <codinginlife@outlook.com>
2020-11-11 15:25:21 -0500

    implement backend

--- backend/count/views.py
+++ backend/count/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from .models import Count;
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt 
 from django.http import JsonResponse
 
 # Create your views here.
-@csrf_protect
+@csrf_exempt 
 @require_http_methods(["GET", "POST"])
 def count(request):
     count = Count.get_solo()
     if request.method == 'POST':
-        count.update(value=count.value + 1)
+        count.value=count.value - 1
+        count.save()
     return JsonResponse({ "count": count.value })
--- backend/djangoangular/settings.py
+++ backend/djangoangular/settings.py
@@ -37,11 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'count',
     'rest_framework',
+    'django_crontab',
+]
+
+CRONJOBS = [
+    ('*/1 * * 0 0', 'count.cron.reset_count')
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +58,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 ROOT_URLCONF = 'djangoangular.urls'
 
 TEMPLATES = [
